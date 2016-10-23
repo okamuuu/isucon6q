21164f36944b653596958f3e5c9315b2f7017aa5
okamuuu <okamuuu@gmail.com>
2016-10-23 14:12:37 +0900

    Trie で正規表現を生成するので SELECT 時に長い順に並べる必要がない

--- perl/lib/Isuda/Web.pm
+++ perl/lib/Isuda/Web.pm
@@ -241,7 +241,7 @@ sub htmlify {
     return '' unless defined $content;
     start('sub htmlify -> select keywords');
     my $keywords = $self->dbh->select_all(qq[
-        SELECT keyword FROM entry ORDER BY CHARACTER_LENGTH(keyword) DESC
+        SELECT keyword FROM entry;
     ]);
     end('sub htmlify -> select keywords');
 
