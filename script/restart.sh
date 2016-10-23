2d1bcea6be45d0a56a66e6e6c1afc630489d1eb7
okamuuu <okamuuu@gmail.com>
2016-10-23 13:39:04 +0900

    sudo systemctl disable isutar.perl.service

--- script/restart.sh
+++ script/restart.sh
@@ -3,5 +3,4 @@ sudo systemctl daemon-reload
 sudo systemctl restart nginx.service
 sudo systemctl restart mysql.service
 sudo systemctl restart isupam.service
-sudo systemctl restart isutar.perl.service
 sudo systemctl restart isuda.perl.service
